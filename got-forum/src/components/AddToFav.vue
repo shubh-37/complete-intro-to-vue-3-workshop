35de5157236fc7e89e67a2048a08c5f4026dad52
Shubh Arya <shubh.arya37@gmail.com>
2024-04-26 13:30:53 +0530

    added 2 components

---
+++ got-forum/src/components/AddToFav.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: {
+    favorites: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+<template>
+  <ul v-if="favorites.length > 0">
+    <li v-for="(character, index) in favorites" :key="`${character}-${index}`">
+      {{ character.name }}
+    </li>
+  </ul>
+  <p v-else>There are no favorite characters yet</p>
+</template>
---
+++ got-forum/src/components/StatisticsForGot.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  props: {
+    characters: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    getStats() {
+      const stats = this.characters.reduce(
+        (acc, item) =>
+          acc[item.house]
+            ? { ...acc, [item.house]: acc[item.house] + 1 }
+            : { ...acc, [item.house]: 1 },
+        {}
+      )
+      return stats
+    }
+  }
+}
+</script>
+<template>
+  <div>
+    <h4>Statistics</h4>
+    <ul>
+      <li v-for="(key, st) in getStats" :key="`${key}-${st}`">{{ st }} : {{ key }}</li>
+    </ul>
+  </div>
+</template>
