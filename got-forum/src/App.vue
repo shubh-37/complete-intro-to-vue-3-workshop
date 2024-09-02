f1afadb4c5e0aee23ba0be50f412d86c45a21374
Shubh Arya <shubh.arya37@gmail.com>
2024-09-02 17:19:10 +0530

    slots demonstration

--- got-forum/src/App.vue
+++ got-forum/src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import StatisticsForGotVue from './components/StatisticsForGot.vue'
 import CharacterCard from './components/CharacterCard.vue'
+import BaseLayout from './components/BaseLayout.vue'
 export default {
   data() {
     return {
@@ -28,13 +29,20 @@ export default {
   },
   components: {
     CharacterCard,
-    StatisticsForGotVue
+    StatisticsForGotVue,
+    BaseLayout
   }
 }
 </script>
 
 <template>
   <div id="app">
+    <base-layout>
+      <template #one>
+        <p>Add a new character</p>
+        <input type="text" v-model="characterName.name" v-on:keyup.enter="addANewCharacter" />
+      </template>
+    </base-layout>
     <statistics-for-got-vue :characters="charactersList" />
     <ul>
       <li v-for="(character, index) in charactersList" :key="`${character}-${index}`">
@@ -48,7 +56,5 @@ export default {
       </li>
     </ul>
     <p v-else>There are no favorite characters yet</p>
-    <p>Add a new character</p>
-    <input type="text" v-model="characterName.name" v-on:keyup.enter="addANewCharacter" />
   </div>
 </template>
---
+++ got-forum/src/components/BaseLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="grid">
+    <div>
+      <slot name="one"></slot>
+    </div>
+    <div>
+      <slot name="two"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  border: 1px solid black;
+}
+</style>
